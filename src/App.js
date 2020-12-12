172385e30eb28a2b1d8b6c4680f38e816efbdfe5
Pablo Vazquez <pvazquez@vandldigital.com>
2020-12-12 17:14:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+import auth from './utility/auth/auth';
+
+import Home from './components/Home/Home';
+import Dash from './components/Dash/Dash';
+import Clients from './components/Clients/Clients';
+import ClientEditor from './components/ClientEditor/ClientEditor';
+import Claims from './components/Claims/Claims';
+import ClaimEditor from '.components/ClaimEditor/ClaimEditor';
+import Policies from './components/Policies/Policies';
+import PolicyEditor from './components/PolicyEditor/PolicyEditor';
+import Notes from './components/Notes/Notes';
+import NoteEditor from './components/NoteEditor/NoteEditor';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <Switch>
+
+                {/* HOME PAGE */}
+                <Route exact path='/' render={()=> {
+                  auth() === false ? <Home/> : <Redirect to={'/dash'}/>
+                }}/>
+
+                <Route exact path='/dash' render={()=> {
+                  auth() === true ? <Dash/> : <Redirect to={'/'}/>
+                }} />
+
+                {/* CLIENT ROUTES */}
+                <Route exact path='/clients' render={()=> {
+                  auth() === true ? <Clients/> : <Redirect to={'/'}/>
+                }} />
+
+                <Route exact path='/clients/:id' render={()=> {
+                  auth() === true ? <ClientEditor/> : <Redirect to={'/'}/>
+                }} />
+                
+                {/* CLAIM ROUTES */}
+                <Route exact path='/claims' render={()=> {
+                  auth() === true ? <Claims/> : <Redirect to={'/'}/>
+                }} />
+
+                <Route exact path='/claims/:id' render={()=> {
+                  auth() === true ? <ClaimEditor/> : <Redirect to={'/'}/>
+                }} />
+
+                {/* POLICY ROUTES */}
+                <Route exact path='/policies' render={()=> {
+                  auth() === true ? <Policies/> : <Redirect to={'/'}/>
+                }} />
+
+                <Route exact path='/policies/:id' render={()=> {
+                  auth() === true ? <PolicyEditor/> : <Redirect to={'/'}/>
+                }} />
+
+                {/* NOTE ROUTES */}
+                <Route exact path='/notes' render={()=> {
+                  auth() === true ? <Notes/> : <Redirect to={'/'}/>
+                }} />
+
+                <Route exact path='/notes/:id' render={()=> {
+                  auth() === true ? <NoteEditor/> : <Redirect to={'/'}/>
+                }} />
+
+            </Switch>
+        </Router>
     </div>
   );
 }
