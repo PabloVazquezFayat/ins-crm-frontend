e358cd12ab93ac898bdc19e59ae5059b85f05547
Pablo Vazquez <pvazquez@vandldigital.com>
2020-12-16 01:13:29 -0500

    experimenting with react-spring transitions

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,9 @@
 import './Signup.css'
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import InputCard from '../InputCard/InputCard'
 import Input from '../Input/Input'
 
-import {useSpring, animated} from 'react-spring'
+import {useSpring, useTransition, animated} from 'react-spring'
 
 export default function Signup() {
 
@@ -30,6 +30,17 @@ export default function Signup() {
         }
     }
 
+    const fadeInBottomUp = (settings)=>{
+        const {duration, delay} = settings;
+        return {
+            transform: "translate(0px, 0px)",
+            opacity: 1,
+            from: {opacity: 0,  transform: "translate(0px, 100px)"}, 
+            delay: delay,
+            config: {duration: duration}
+        }
+    }
+
     const step1 = useSpring(fadeIn({duration: 200, delay: 0}));
     const step2 = useSpring(fadeIn({duration: 200, delay: 200}));
     const step3 = useSpring(fadeIn({duration: 200, delay: 400}));
@@ -37,7 +48,19 @@ export default function Signup() {
     const connectorStep1 = useSpring(expandDown({duration: 200, delay: 800}));
     const connectorStep2 = useSpring(expandDown({duration: 200, delay: 1100}));
 
-    const signup = ()=> console.log('Signup');
+    const infoCard1 = useSpring(fadeInBottomUp({duration: 200, delay: 1400}));
+
+    const [card, setCard] = useState(0);
+
+    const hidden = {
+        display: "none"
+    }
+
+    const next = ()=>{
+        setCard((card + 1) % 3);
+    }
+
+    const submit = ()=> console.log('submit');
 
     return (
         <div className="sign-up-page">
@@ -78,24 +101,30 @@ export default function Signup() {
                     </div>
 
                     <div className="sign-up-form-wrapper">
-                        
-                        <div  id="slide-1">
-                            <InputCard header="sign up" button="next" action={signup}>
+
+                        <animated.div style={card === 0 ? infoCard1 : hidden}>
+                            <InputCard header="sign up" button="next" action={next}>
                                 <Input type="text" name="first-name" id="sign-up-first-name" placeholder="First name"/>
                                 <Input type="text" name="last-name" id="sign-up-last-name" placeholder="Last name"/>
                                 <Input type="email" name="email" id="sign-up-email" placeholder="Email"/>
                                 <Input type="password" name="password" id="sign-up-password" placeholder="Password"/>
                             </InputCard>
-                        </div>
+                        </animated.div>
 
-                        <div  id="slide-2">
-                            <InputCard header="payment method" button="next" action={signup}>
+                        <animated.div style={card === 1 ? infoCard1 : hidden}>
+                            <InputCard header="payment method" button="next" action={next}>
                                 <Input type="text" name="cc-name" id="sign-up-cc-name" placeholder="Name on credit card"/>
                                 <Input type="number" name="cc-number" id="sign-up-cc-number" minLength="16" maxLength="16" placeholder="0000-0000-0000-0000"/>
                                 <Input type="text" name="cc-expiry" id="sign-up-cc-expiry" minLength="7" maxLength="7" placeholder="mm/yyyy"/>
                                 <Input type="number" name="cc-security-code" id="sign-up-cc-security-code" minLength="3" maxLength="3" placeholder="000"/>
                             </InputCard>
-                        </div>
+                        </animated.div> 
+
+                        <animated.div style={card === 2 ? infoCard1 : hidden}>
+                            <InputCard header="payment method" button="submit" action={submit}>
+                                REVIEW
+                            </InputCard>
+                        </animated.div> 
 
                     </div>
 
