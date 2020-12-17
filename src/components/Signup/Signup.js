b4945d06dfa5601aeb11671424731deab5f323b9
Pablo Vazquez <pvazquez@vandldigital.com>
2020-12-17 00:56:13 -0500

    sign up steps final animations using react-spring

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -30,26 +30,6 @@ export default function Signup() {
         }
     }
 
-    const slideUp = (settings)=> {
-        const {duration, delay} = settings;
-        return {
-            marginTop: '0px',
-            from: {marginTop: '360px'}, 
-            delay: delay,
-            config: {duration: duration}
-        }
-    }
-
-    const fade = (settings)=>{
-        const {duration, delay} = settings;
-        return {
-            opacity: 1,
-            from: {opacity: 0}, 
-            delay: delay,
-            config: {duration: duration}
-        }
-    }
-
     const step1 = useSpring(fadeIn({duration: 200, delay: 0}));
     const step2 = useSpring(fadeIn({duration: 200, delay: 200}));
     const step3 = useSpring(fadeIn({duration: 200, delay: 400}));
@@ -57,42 +37,46 @@ export default function Signup() {
     const connectorStep1 = useSpring(expandDown({duration: 200, delay: 800}));
     const connectorStep2 = useSpring(expandDown({duration: 200, delay: 1100}));
 
-    const cardDeck = useSpring(slideUp({duration: 200, delay: 1100}));
-    const card1 = useSpring(fade({duration: 300, delay: 1200}));
-    const card2 = useSpring(fade({duration: 200, delay: 1100}));
+    const [card, setCard] = useState("0px");
+    const [card1, setCard1] = useState(1);
+    const [card2, setCard2] = useState(0);
+    const [card3, setCard3] = useState(0);
 
-    const [card, setCard] = useState(0);
+    const fistCard = useSpring({
+        opacity: 1, 
+        transform: "translate(0px, 0px)",
+        from: {opacity: 0, transform: "translate(0px, 360px)"},
+        delay: 1600,
+        config: {duration: 300}
+    })
 
-    console.log(card);
+    const cardDeck = useSpring({transform: `translate(0px, ${card})`});
+
+    const toggleCard1 = useSpring({opacity: card1});
+    const toggleCard2 = useSpring({opacity: card2});
+    const toggleCard3 = useSpring({opacity: card3});
 
     const next = ()=> console.log('next')//setCard(state => (state + 1) % 3);
 
-    const CurrentCard = ()=>{
-        if(card === 0){
-            return  <animated.div style={card1}>
-                        <InputCard header="sign up" button="next" action={next}>
-                            <Input type="text" name="first-name" id="sign-up-first-name" placeholder="First name"/>
-                            <Input type="text" name="last-name" id="sign-up-last-name" placeholder="Last name"/>
-                            <Input type="email" name="email" id="sign-up-email" placeholder="Email"/>
-                            <Input type="password" name="password" id="sign-up-password" placeholder="Password"/>
-                        </InputCard>
-                    </animated.div>
-        }else if(card === 1){
-            return  <animated.div style={card2}>
-                        <InputCard header="payment method" button="next" action={next}>
-                            <Input type="text" name="cc-name" id="sign-up-cc-name" placeholder="Name on credit card"/>
-                            <Input type="number" name="cc-number" id="sign-up-cc-number" minLength="16" maxLength="16" placeholder="0000-0000-0000-0000"/>
-                            <Input type="text" name="cc-expiry" id="sign-up-cc-expiry" minLength="7" maxLength="7" placeholder="mm/yyyy"/>
-                            <Input type="number" name="cc-security-code" id="sign-up-cc-security-code" minLength="3" maxLength="3" placeholder="000"/>
-                        </InputCard>
-                    </animated.div> 
-        }else if(card === 2){
-            return  <animated.div style={card1}>
-                        <InputCard header="payment method" button="submit" action={next}>
-                            REVIEW
-                        </InputCard>
-                    </animated.div>
-        }
+    const gotToCard1 = ()=> {
+        setCard("0px"); 
+        setCard1(1);
+        setCard2(0);
+        setCard3(0);
+    }
+
+    const gotToCard2 = ()=> {
+        setCard("-320px"); 
+        setCard1(0);
+        setCard2(1);
+        setCard3(0);
+    }
+
+    const gotToCard3 = ()=> {
+        setCard("-640px"); 
+        setCard1(0);
+        setCard2(0);
+        setCard3(1);
     }
 
     return (
@@ -105,7 +89,7 @@ export default function Signup() {
                         <animated.div style={step1}>
                             <div className="breadcrumb-step">
                                 <p className="breadcrumb-text">User details</p>
-                                <div className="breadcrumb-circle" onClick={()=> setCard(0)}>1</div>
+                                <div className="breadcrumb-circle" onClick={gotToCard1}>1</div>
                             </div>
                         </animated.div>
                         
@@ -116,7 +100,7 @@ export default function Signup() {
                         <animated.div style={step2}>
                             <div className="breadcrumb-step">
                                 <p className="breadcrumb-text">Payment method</p>
-                                <div className="breadcrumb-circle" onClick={()=> setCard(1)}>2</div>
+                                <div className="breadcrumb-circle" onClick={gotToCard2}>2</div>
                             </div>
                         </animated.div>
                         
@@ -127,19 +111,39 @@ export default function Signup() {
                         <animated.div style={step3}>
                             <div className="breadcrumb-step">
                                 <p className="breadcrumb-text">Review</p>
-                                <div className="breadcrumb-circle" onClick={()=> setCard(2)}>3</div>
+                                <div className="breadcrumb-circle" onClick={gotToCard3}>3</div>
                             </div>
                         </animated.div>
                         
                     </div>
 
-                    <div className="sign-up-form-wrapper">
+                    <animated.div style={fistCard} className="sign-up-form-wrapper">
+                        <animated.div style={cardDeck}>
+                            <animated.div style={toggleCard1}>
+                                <InputCard header="sign up" button="next" action={gotToCard2}>
+                                    <Input type="text" name="first-name" id="sign-up-first-name" placeholder="First name"/>
+                                    <Input type="text" name="last-name" id="sign-up-last-name" placeholder="Last name"/>
+                                    <Input type="email" name="email" id="sign-up-email" placeholder="Email"/>
+                                    <Input type="password" name="password" id="sign-up-password" placeholder="Password"/>
+                                </InputCard>
+                            </animated.div>
 
-                        <animated.div>
-                           <CurrentCard/>
+                            <animated.div style={toggleCard2}>
+                                <InputCard header="payment method" button="next" action={gotToCard3}>
+                                    <Input type="text" name="cc-name" id="sign-up-cc-name" placeholder="Name on credit card"/>
+                                    <Input type="number" name="cc-number" id="sign-up-cc-number" minLength="16" maxLength="16" placeholder="0000-0000-0000-0000"/>
+                                    <Input type="text" name="cc-expiry" id="sign-up-cc-expiry" minLength="7" maxLength="7" placeholder="mm/yyyy"/>
+                                    <Input type="number" name="cc-security-code" id="sign-up-cc-security-code" minLength="3" maxLength="3" placeholder="000"/>
+                                </InputCard>
+                            </animated.div> 
+
+                            <animated.div style={toggleCard3}>
+                                <InputCard header="payment method" button="submit" action={next}>
+                                    REVIEW
+                                </InputCard>
+                            </animated.div>
                         </animated.div>
-
-                    </div>
+                    </animated.div>
 
                 </div>
             </div>
